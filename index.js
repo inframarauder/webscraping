8ad6bf9067edbc14053a05393b8ff5ae7a8db3bd
Subhasis Das <40117358+subhasis020299@users.noreply.github.com>
2019-05-28 17:35:07 +0530

    scraped VVIP links

--- index.js
+++ index.js
@@ -1,21 +1,34 @@
-const scrape = require('./scrape');
-const express = require('express');
-const bodyparser = require('body-parser');
+const scrape = require("./scrape");
+const express = require("express");
+const bodyparser = require("body-parser");
 
 const app = express();
 
-app.use(bodyparser.urlencoded({extended:false}));
+app.use(bodyparser.urlencoded({ extended: false }));
 
-app.get('/',(req,res)=>{
-    scrape.then(noticeArray=>{
+app.get("/:id", (req, res) => {
+  if (req.params.id === "notices") {
+    scrape.scrapeNoticeBoard
+      .then(noticeArray => {
         return res.send(noticeArray);
-    }).catch(err=>{
+      })
+      .catch(err => {
         return res.send(err);
-    });
+      });
+  }
+  if (req.params.id === "links") {
+    scrape.scrapeVVIP
+      .then(linksArray => {
+        return res.send(linksArray);
+      })
+      .catch(err => {
+        return res.send(err);
+      });
+  }
 });
 
 var port = process.env.PORT || 8080;
 
 app.listen(port);
 
-console.log('listening to port '+port);
+console.log("listening to port " + port);
